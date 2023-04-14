ce4a37588216a3adb2ae7a0e00475d7b2fc16a03
Ed <eborgula@umich.edu>
2023-04-13 22:36:47 -0400

    Fixed Initial 2

--- src/testing/testSelectionSort.java
+++ src/testing/testSelectionSort.java
@@ -1,5 +1,11 @@
 package testing;
 
+/*
+* testSelectionSort.java
+*
+* To change this template, choose Tools | Template Manager
+* and open the template in the editor.
+*/
 import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,14 +32,15 @@ Sortedarr[1] = 7;
 Sortedarr[2] = 8;
 Sortedarr[3] = 9;
 Sortedarr[4] = 10;
-	SelectionSort temp1 = new SelectionSort();
-	arr = temp1.basicSelectionSort(arr);
 /** add tests to check for this unit test **/
-assertEquals("Wrong pos 0", Sortedarr[0], arr[0]);
-assertEquals("Wrong pos 1", Sortedarr[1], arr[1]);
-assertEquals("Wrong pos 2", Sortedarr[2], arr[2]);
-assertEquals("Wrong pos 3", Sortedarr[3], arr[3]);
-assertEquals("Wrong pos 4", Sortedarr[4], arr[4]);
+SelectionSort temp1 = new SelectionSort();
+arr = temp1.basicSelectionSort(arr);
+assertEquals("Wrong", Sortedarr[0], arr[0]);
+assertEquals("Wrong", Sortedarr[1], arr[1]);
+assertEquals("Wrong", Sortedarr[2], arr[2]);
+assertEquals("Wrong", Sortedarr[3], arr[3]);
+assertEquals("Wrong", Sortedarr[4], arr[4]);
+
 }
 public void testNegative(){
 	int[] arr = new int[5];
@@ -48,14 +55,15 @@ public void testNegative(){
 	Sortedarr[2] = -8;
 	Sortedarr[3] = -7;
 	Sortedarr[4] = -2;
+	/** add tests to check for this unit test **/
 	SelectionSort temp1 = new SelectionSort();
 	arr = temp1.basicSelectionSort(arr);
+	assertEquals("Wrong", Sortedarr[0], arr[0]);
+	assertEquals("Wrong", Sortedarr[1], arr[1]);
+	assertEquals("Wrong", Sortedarr[2], arr[2]);
+	assertEquals("Wrong", Sortedarr[3], arr[3]);
+	assertEquals("Wrong", Sortedarr[4], arr[4]);
 /** Test data contains negative values only **/
-	assertEquals("Wrong neg 0", Sortedarr[0], arr[0]);
-	assertEquals("Wrong neg 1", Sortedarr[1], arr[1]);
-	assertEquals("Wrong neg 2", Sortedarr[2], arr[2]);
-	assertEquals("Wrong neg 3", Sortedarr[3], arr[3]);
-	assertEquals("Wrong neg 4", Sortedarr[4], arr[4]);
 }
 public void testMixed(){
 	int[] arr = new int[5];
@@ -70,19 +78,20 @@ public void testMixed(){
 	Sortedarr[2] = 0;
 	Sortedarr[3] = 2;
 	Sortedarr[4] = 8;
+	/** add tests to check for this unit test **/
 	SelectionSort temp1 = new SelectionSort();
 	arr = temp1.basicSelectionSort(arr);
+	assertEquals("Wrong", Sortedarr[0], arr[0]);
+	assertEquals("Wrong", Sortedarr[1], arr[1]);
+	assertEquals("Wrong", Sortedarr[2], arr[2]);
+	assertEquals("Wrong", Sortedarr[3], arr[3]);
+	assertEquals("Wrong", Sortedarr[4], arr[4]);
 /** Test data contains with both positive, negative and zeros **/
-	assertEquals("Wrong mix 0", Sortedarr[0], arr[0]);
-	assertEquals("Wrong mix 1", Sortedarr[1], arr[1]);
-	assertEquals("Wrong mix 2", Sortedarr[2], arr[2]);
-	assertEquals("Wrong mix 3", Sortedarr[3], arr[3]);
-	assertEquals("Wrong mix 4", Sortedarr[4], arr[4]);
 }
 public void testDuplicates(){
 	int[] arr = new int[5];
-	arr[0] = 8;
-	arr[1] = 7;
+	arr[0] = 7;
+	arr[1] = 9;
 	arr[2] = 7;
 	arr[3] = 10;
 	arr[4] = 2;
@@ -90,15 +99,16 @@ public void testDuplicates(){
 	Sortedarr[0] = 2;
 	Sortedarr[1] = 7;
 	Sortedarr[2] = 7;
-	Sortedarr[3] = 8;
+	Sortedarr[3] = 9;
 	Sortedarr[4] = 10;
+	/** add tests to check for this unit test **/
 	SelectionSort temp1 = new SelectionSort();
 	arr = temp1.basicSelectionSort(arr);
+	assertEquals("Wrong", Sortedarr[0], arr[0]);
+	assertEquals("Wrong", Sortedarr[1], arr[1]);
+	assertEquals("Wrong", Sortedarr[2], arr[2]);
+	assertEquals("Wrong", Sortedarr[3], arr[3]);
+	assertEquals("Wrong", Sortedarr[4], arr[4]);
 /** Test data contains duplicates **/
-	assertEquals("Wrong dupe 0", Sortedarr[0], arr[0]);
-	assertEquals("Wrong dupe 1", Sortedarr[1], arr[1]);
-	assertEquals("Wrong dupe 2", Sortedarr[2], arr[2]);
-	assertEquals("Wrong dupe 3", Sortedarr[3], arr[3]);
-	assertEquals("Wrong dupe 4", Sortedarr[4], arr[4]);
 }
 }
